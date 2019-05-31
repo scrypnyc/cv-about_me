c28cb715f7197d451fd336b5881ad322652bd0e0
Alexander <alexanderlookua@gmail.com>
2019-05-31 12:12:32 +0300

    Initial Commit

---
+++ cv-about_me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cv-about_me
+//
+//  Created by Alexander Skrypnyk on 5/31/19.
+//  Copyright © 2019 Alexander Skrypnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
